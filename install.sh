3d33020a3f7b2ce8f4b7c96ab74928fe8557def5
Hermes Mesquita <hermesmesquita@gmail.com>
2025-07-03 19:29:13 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,19 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 
 source ~/.bashrc
 
+sudo apt-get install gnupg curl
+curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg \
+   --dearmor
+
+echo "deb [ signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] http://repo.mongodb.org/apt/debian bookworm/mongodb-org/8.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list
+
+sudo apt-get update
+
+sudo apt-get install -y mongodb-org
+sudo systemctl start mongod
+
+
 nvm install 24
 
 source ~/.bashrc
