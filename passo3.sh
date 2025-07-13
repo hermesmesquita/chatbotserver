0ec1d3b314125159281b89dbea393ea291f4509a
Hermes Mesquita <hermesmesquita@gmail.com>
2025-07-13 14:08:18 -0300

    Update passo3.sh

--- passo3.sh
+++ passo3.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 docker volume create n8n_data 
-docker run -it --rm --name n8n -p 5678:5678 -v n8n_data:/home/node/.n8n docker.n8n.io/n8nio/n8n
+docker run -it --name n8n -p 5678:5678 -v n8n_data:/home/node/.n8n -restart unless-stopped docker.n8n.io/n8nio/n8n
+
+sudo wget https://raw.githubusercontent.com/hermesmesquita/chatbotserver/refs/heads/main/n8n.service -O /etc/systemd/system/n8n.service
+
+sudo systemctl daemon-reload
+
+sudo reboot
