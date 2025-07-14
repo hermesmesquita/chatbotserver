8fff143ad9174490b494d7f31e509e62850a2830
Hermes Mesquita <hermesmesquita@gmail.com>
2025-07-14 15:27:36 -0300

    Update passo3.sh

--- passo3.sh
+++ passo3.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 docker volume create n8n_data 
-docker volume create sessions
-docker volume create media
+docker volume create waha_sessions
+docker volume create waha_media
 docker run --name n8n   -dp 5678:5678 -v n8n_data:/home/node/.n8n --env-file .env     --restart unless-stopped docker.n8n.io/n8nio/n8n
-docker run --name waha  -dp 3000:3000 -v sessions:/app/.sessions -v media:/app/.media --restart unless-stopped devlikeapro/waha
+docker run --name waha  -dp 3000:3000 -v waha_sessions:/app/.sessions -v waha_media:/app/.media --restart unless-stopped devlikeapro/waha
 
 sudo wget https://raw.githubusercontent.com/hermesmesquita/chatbotserver/refs/heads/main/n8n.service -O /etc/systemd/system/n8n.service
 
