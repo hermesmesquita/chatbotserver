4712dba82f32194ff46f4ec902064d5ec46093b1
Hermes Mesquita <hermesmesquita@gmail.com>
2025-07-16 16:29:04 -0300

    Update passo1.sh

--- passo1.sh
+++ passo1.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 sudo apt-get update
 
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-
 sudo apt-get install nginx snapd gnupg git -y
 
 sudo snap install snapd
@@ -12,6 +10,6 @@ sudo ln -s /snap/bin/certbot /usr/bin/certbot
 echo "*************************************************************"
 echo "CERTBOT INSTALADO. EXECUTE NO TERMINAL O SEGUINTE COMANDO"
 echo ""
-echo "  sudo certbot --nginx -d usina.dev.br -d n8n.usina.dev.br -d waha.usina.dev.br"
+echo "  sudo certbot --nginx -d hermes.dev.br -d n8n.hermes.dev.br -d waha.hermes.dev.br "
 echo ""
 echo "*************************************************************"
