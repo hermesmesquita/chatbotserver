58e2a9c69bf9be6cab20b5587a38bea168c950a6
Hermes Mesquita <hermesmesquita@gmail.com>
2025-07-19 23:40:35 -0300

    Update stepone.sh

--- stepone.sh
+++ stepone.sh
@@ -3,4 +3,11 @@ sudo apt-get update
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 sudo usermod -aG docker $USER
+
+mkdir -p proxy-traefik
+cd proxy-traefik
+touch acme.json
+chmod 600 acme.json
+
+
 sudo reboot
